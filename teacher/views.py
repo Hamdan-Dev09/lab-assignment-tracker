1231c9e991680ca02f4c79ef629e4a538eae5391
Hamdan Zaheer <shade0987insane@gmail.com>
2022-11-17 12:48:00 +0530

    url added

--- teacher/views.py
+++ teacher/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def dashboard(request):
-    return render(request, 'Navbar.html')
+    return render(request, 'Dashboard.html')
+
+def course(request):
+    return render(request, 'Course.html')
+
+def student(request):
+    return render(request, 'student-section.html')
