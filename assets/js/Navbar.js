c4abbc06dae91282d9b6a3282ca8857d3612bab5
Hamdan Zaheer <shade0987insane@gmail.com>
2022-11-17 12:48:00 +0530

    url added

---
+++ assets/js/Navbar.js
@@ -0,0 +1,20 @@
+const nav = document.querySelector('.navigation');
+const navClose = document.querySelector('.nav-close');
+const navOpen = document.querySelector('.burger');
+
+navClose.addEventListener('click', ()=>{
+    nav.classList.remove('nav-active');
+})
+
+navOpen.addEventListener('click', () =>{
+    nav.classList.toggle('nav-active');
+})
+
+
+const colors = ["#4abdac", "#fc4a1a", "#f78733", "#4717f6", "#a239ca", "#062f4f", "#813772", "#ff3b3f", "#76323f"];
+
+const backGroundColor = document.querySelector('.bg');
+
+let randomNumber = Math.floor(Math.random() * colors.length);
+console.log(randomNumber + "   " + colors[randomNumber]);
+backGroundColor.style.backgroundColor = colors[randomNumber];
--- teacher/urls.py
+++ teacher/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name = 'dashboard'),
+    path('Course', views.course, name = 'course'),
+    path('Course/Student', views.student, name = 'student-section'),
 ]
