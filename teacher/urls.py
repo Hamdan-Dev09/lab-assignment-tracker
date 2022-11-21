c1169b5f782b52d2f596308f35c23ea1a91c1282
Hamdan Zaheer <shade0987insane@gmail.com>
2022-11-21 17:17:05 +0530

    login view

--- teacher/urls.py
+++ teacher/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name = 'dashboard'),
+    path('', views.login, name = 'login'),
+    path('teacher', views.dashboard, name = 'dashboard'),
     path('Course', views.course, name = 'course'),
     path('Course/Student', views.student, name = 'student-section'),
 ]
--- teacher/views.py
+++ teacher/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username = username, password = password)
+
+        if user is not None:
+            print("logged In")
+            auth.login(request, user)
+            return redirect('teacher')
+        else:
+            print("1 In")
+            return redirect('/')
+    else:
+        return render(request, 'login.html')
+
+
+
 def dashboard(request):
     return render(request, 'Dashboard.html')
 
